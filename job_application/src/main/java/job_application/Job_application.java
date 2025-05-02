0d2dc74d9b886f8de6da2d46aaba649cf54e06f4
hoangvuhhbg <82321578+hoangvuhhbg@users.noreply.github.com>
2025-05-03 00:24:59 +0700

    Add files via upload

---
+++ job_application/src/main/java/job_application/Job_application.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package job_application;
+
+/**
+ *
+ * @author hoang
+ */
+public class Job_application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
